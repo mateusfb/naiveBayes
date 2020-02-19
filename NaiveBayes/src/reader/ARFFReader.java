9577cc0e1b479565fc750a40a490da5d359f6d67
mateusfb <mateusfirminob@gmail.com>
2020-02-18 21:16:18 -0300

    Readers finalizados

--- NaiveBayes/src/reader/ARFFReader.java
+++ NaiveBayes/src/reader/ARFFReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import naiveBayes.Dataset;
 import naiveBayes.ObjectInstance;
@@ -15,24 +16,22 @@ public class ARFFReader implements Reader {
 	@Override
 	public Dataset read(String path) throws IOException {
 		Dataset dataset = new Dataset(path);
-		ArrayList<String> attributes = new ArrayList<String>();
 		BufferedReader br = new BufferedReader(new FileReader(path));
 		ArffReader arff = new ArffReader(br);
 		
 		Instances data = arff.getData();
+		String[] attributes = new String[data.numAttributes() - 1];
 		
 		if(data.classIndex() == -1) {
 			data.setClassIndex(data.numAttributes() - 1);
 		}
 
 		for(int i = 0; i < data.numInstances(); i++) {
-			
-			attributes.clear();
-			
+		
 			for(int j = 0; j < data.numAttributes() -1; j++) {
-				attributes.add(String.valueOf(data.get(i).value(j)));
+				attributes[j] = String.valueOf(data.get(i).value(j));
 			}
-			dataset.getInstances().add(new ObjectInstance(attributes, data.get(i).classAttribute().name()));
+			dataset.getInstances().add(new ObjectInstance(new ArrayList<String>(Arrays.asList(attributes)), String.valueOf(data.get(i).value(data.classIndex()))));
 		}
 		
 		return dataset;
