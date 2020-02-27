1d60872bca9007c3d10b9625994d50666d897031
mateusfb <mateusfirminob@gmail.com>
2020-02-27 16:29:32 -0300

    Mudanças de alguns arrays para ArrayList

--- NaiveBayes/src/naiveBayes/Main.java
+++ NaiveBayes/src/naiveBayes/Main.java
@@ -2,22 +2,26 @@ package naiveBayes;
 
 import java.io.IOException;
 
+//import reader.ARFFReader;
 import reader.CSVReader;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		Dataset datasetCsv;
+		//Dataset datasetArff;
 		NaiveBayes nb = new NaiveBayes();
 		
-		System.out.println("-----------------CSV-----------------");
+		//System.out.println("-----------------CSV-----------------");
 		CSVReader csvReader = new CSVReader();
-		datasetCsv = csvReader.read("src/resources/PlayGolf.csv");
-		System.out.println(datasetCsv);
+		//datasetCsv = csvReader.read("src/resources/PlayGolf.csv");
+		datasetCsv = csvReader.read("src/resources/teste.csv");
+		//System.out.println(datasetCsv);
 		
 		nb.buildClassifier(datasetCsv);
-		nb.test(datasetCsv, new String[] {"Rainy", "Hot", "High"});
-		
+		//nb.classify(datasetCsv, new String[] {"Rainy", "Hot", "High"});
+		nb.classify(datasetCsv, new String[] {"3.5","3"});
+		//nb.test(datasetCsv);
 	}
 
 }
--- NaiveBayes/src/naiveBayes/NaiveBayes.java
+++ NaiveBayes/src/naiveBayes/NaiveBayes.java
@@ -45,12 +45,12 @@ public class NaiveBayes {
 		return separated;
 	}
 
-	public double[] statisticsByColumn(ArrayList<ObjectInstance> classInstances, int column, String label) {
-		double[] columnData = new double[classOccurances.get(label)];
+	public double[] statisticsByColumn(ArrayList<ObjectInstance> classInstances, int column) {
+		ArrayList<Double> columnData = new ArrayList<Double>();
 		double[] statistics = new double[2];
 		
 		for(int i = 0; i < classInstances.size(); i++) {
-			columnData[i] = Double.parseDouble(classInstances.get(i).getAttributes().get(column));
+			columnData.add(Double.parseDouble(classInstances.get(i).getAttributes().get(column)));
 		}
 		
 		statistics[0] = Statistics.mean(columnData);
@@ -71,32 +71,61 @@ public class NaiveBayes {
 		return count / (double) classInstances.size();
 	}
 	
-	public void test(Dataset dataset, String[] tested) {
-		double[] classProbabilities = new double[dataset.getNumClass()];
-		double[] attributeProbabilities = new double[dataset.getNumAttributes()];
+	public ArrayList<Double> calculateClassProbabilities(Dataset dataset, String[] tested) {
+		ArrayList<Double> classProbabilities = new ArrayList<Double>();
+		ArrayList<Double> attributeProbabilities = new ArrayList<Double>();
 		double[] statistics;
 		ArrayList<ArrayList<ObjectInstance>> separated = separateByClass(dataset);
 		
 		for(int i = 0; i < dataset.getNumClass(); i++) { //i representa as classes
 			for(int j = 0; j < dataset.getNumAttributes(); j++) { //j representa os atributos
 				if(dataset.getDataTypes()[j] == 0) {
-					statistics = statisticsByColumn(separated.get(i), j, classes[i]);
-					attributeProbabilities[j] = Statistics.gaussianPdf(Double.parseDouble(tested[j]), statistics[0], statistics[1]);
+					statistics = statisticsByColumn(separated.get(i), j);
+					System.out.println(statistics[0]);
+					System.out.println(statistics[1]);
+					attributeProbabilities.add(Statistics.gaussianPdf(Double.parseDouble(tested[j]), statistics[0], statistics[1]));
 				} else {
-					attributeProbabilities[j] = probabilityOfNominalAttribute(separated.get(i), j, tested[j]);
+					attributeProbabilities.add(probabilityOfNominalAttribute(separated.get(i), j, tested[j]));
 				}
 			}
-			classProbabilities[i] = Statistics.multiplyArray(attributeProbabilities) * ((double)classOccurances.get(classes[i]) / dataset.getInstances().size());
+			classProbabilities.add(Statistics.multiplyArrayList(attributeProbabilities) * ((double)classOccurances.get(classes[i]) / dataset.getInstances().size()));
+			attributeProbabilities.clear();
 		}
 		
-		printResult(classProbabilities);
+		return classProbabilities;
 	}
 	
-	public void printResult(double[] classProbabilities) {
-		double sum = Statistics.sumArray(classProbabilities);
+	public void classify(Dataset dataset, String[] tested) {
+		printResult(calculateClassProbabilities(dataset, tested));
+	}
+	
+	public void printResult(ArrayList<Double> classProbabilities) {
+		double sum = Statistics.sumArrayList(classProbabilities);
 		
 		for(int i = 0; i < classes.length; i++) {
-			System.out.println(classes[i] + ": " + (classProbabilities[i] / sum)*100 + "%");
+			System.out.println(classes[i] + ": " + (classProbabilities.get(i) / sum)*100 + "%");
 		}
+	}
+	
+	public void test(Dataset dataset) {
+		double correct = 0;
+		double incorrect = 0;
+		
+		for(ObjectInstance instance : dataset.getInstances()) {
+			String[] attributes = new String[dataset.getNumAttributes()];
+			attributes = instance.getAttributes().toArray(attributes);
+			ArrayList<Double> instanceProbabilities = calculateClassProbabilities(dataset, attributes);
+			
+			int maxIdx = Statistics.findMaxIdx(instanceProbabilities);
+			
+			if(instance.getLabel().equals(classes[maxIdx])) {
+				correct++;
+			} else {
+				incorrect++;
+			}
+		}
+		
+		System.out.println("correct: " + correct / dataset.getInstances().size());
+		System.out.println("incorrect: " + incorrect / dataset.getInstances().size());
 	}
 }
